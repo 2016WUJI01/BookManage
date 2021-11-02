848c57f8972ab62c88d1497990cc6e2c7876e64c
吴嘉顺 <870435170@qq.com>
2021-11-03 00:40:29 +0800

    Update test-vue.sql

--- test-vue.sql
+++ test-vue.sql
@@ -10,10 +10,90 @@ Target Server Type    : MYSQL
 Target Server Version : 50731
 File Encoding         : 65001
 
-Date: 2021-10-29 21:46:25
+Date: 2021-11-03 00:38:51
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `back`
+-- ----------------------------
+DROP TABLE IF EXISTS `back`;
+CREATE TABLE `back` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `adminid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `bookid` int(11) NOT NULL,
+  `price` double(5,2) NOT NULL,
+  `number` int(6) NOT NULL,
+  `date` date NOT NULL,
+  `reason` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of back
+-- ----------------------------
+INSERT INTO `back` VALUES ('1', '3', '2019000001', '1001', '10.55', '20', '2021-11-02', '多余');
+
+-- ----------------------------
+-- Table structure for `book`
+-- ----------------------------
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bookname` varchar(255) COLLATE utf8_bin NOT NULL,
+  `supplier` varchar(16) COLLATE utf8_bin NOT NULL,
+  `price` double(5,2) NOT NULL,
+  `reserve` int(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1003 DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of book
+-- ----------------------------
+INSERT INTO `book` VALUES ('1001', '数学教科书', '新华书店', '15.90', '1');
+INSERT INTO `book` VALUES ('1002', '英语教科书', '新华书店', '14.90', '2');
+
+-- ----------------------------
+-- Table structure for `order`
+-- ----------------------------
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL,
+  `adminid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `bookid` int(11) NOT NULL,
+  `number` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `situation` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of order
+-- ----------------------------
+INSERT INTO `order` VALUES ('1', '1', '2019000001', '1001', '20', '2019-01-01', '未完成');
+
+-- ----------------------------
+-- Table structure for `stuclass`
+-- ----------------------------
+DROP TABLE IF EXISTS `stuclass`;
+CREATE TABLE `stuclass` (
+  `stuclass` int(11) NOT NULL,
+  `college` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `classname` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`stuclass`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of stuclass
+-- ----------------------------
+INSERT INTO `stuclass` VALUES ('2000000', '图书管理处', '图书管理处');
+INSERT INTO `stuclass` VALUES ('2019001', '大数据与软件工程学院', '计算机191');
+INSERT INTO `stuclass` VALUES ('2019002', '大数据与软件工程学院', '计算机192');
+INSERT INTO `stuclass` VALUES ('2019003', '大数据与软件工程学院', '计算机193');
+INSERT INTO `stuclass` VALUES ('2019004', '大数据与软件工程学院', '软件工程191');
 
 -- ----------------------------
 -- Table structure for `user`
@@ -23,11 +103,21 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) COLLATE utf8_bin NOT NULL,
   `password` varchar(255) COLLATE utf8_bin NOT NULL,
+  `nickname` varchar(255) COLLATE utf8_bin NOT NULL,
+  `sex` int(1) NOT NULL,
+  `phonenumber` varchar(11) COLLATE utf8_bin NOT NULL,
+  `stuclass` int(16) NOT NULL,
+  `position` varchar(16) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2019000004 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'admin', 'admin');
-INSERT INTO `user` VALUES ('2', 'root', 'root');
+INSERT INTO `user` VALUES ('1', 'admin', 'admin', 'admin', '1', '13800000000', '2000000', '管理员');
+INSERT INTO `user` VALUES ('2', 'root', 'root', 'admin', '1', '13800000001', '2000000', '管理员');
+INSERT INTO `user` VALUES ('3', '赵一', '123456', 'admin', '1', '13800000000', '2000000', '管理员');
+INSERT INTO `user` VALUES ('4', '钱二', '1234567', 'admin', '1', '13866816590', '2000000', '管理员');
+INSERT INTO `user` VALUES ('2019000001', '金泰相', '123456', 'doinb', '1', '19800000000', '2019001', '学生');
+INSERT INTO `user` VALUES ('2019000002', 'henry', 'root123', 'henry', '1', '19800000001', '2019001', '学生');
+INSERT INTO `user` VALUES ('2019000003', 'jy', '123456', 'jy', '0', '12333333333', '2019001', '教师');
