5550d0179f63e001d349a62478740f212101a427
吴嘉顺 <870435170@qq.com>
2021-11-19 23:28:13 +0800

    Update User.vue

--- test-vue/src/components/user/User.vue
+++ test-vue/src/components/user/User.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div style="float: right">
-      <el-button @click="showCreate">新增</el-button>
+      <el-button>新增</el-button>
     </div>
 
     <div>
@@ -57,15 +57,10 @@
             <el-button
               size="mini"
               type="primary"
-              @click="showUpdate(scope.$index)"
-              v-permission="'user:userdate'"
             >修改</el-button>
             <el-button
               size="mini"
               type="danger"
-              v-if="scope.row.id !== id"
-              @click="removeUser(scope.$indexx)"
-              v-permission="'user:userdate'"
             >删除</el-button>
           </template>
         </el-table-column>
@@ -82,120 +77,7 @@
       >
       </el-pagination>
     </div>
-    <el-dialog
-      :title="textMap[dialogStatus]"
-      :visible.sync="dialogFormVisible"
-    >
-      <el-form
-        class="small-space"
-        :model="tempUser"
-        label-position="left"
-        label-width="80px"
-        style='width: 300px; margin-left:50px;'
-      >
-        <el-form-item
-          label="Id"
-          required
-          v-if="dialogStatus==='create'"
-        >
-          <el-input
-            type="text"
-            v-model="tempUser.id"
-          >
-          </el-input>
-        </el-form-item>
-        <el-form-item
-          label="用户名"
-          required
-          v-if="dialogStatus==='create'"
-        >
-          <el-input
-            type="text"
-            v-model="tempUser.username"
-          >
-          </el-input>
-        </el-form-item>
-        <el-form-item
-          label="密码"
-          v-if="dialogStatus==='create'"
-          required
-        >
-          <el-input
-            type="password"
-            v-model="tempUser.password"
-          >
-          </el-input>
-        </el-form-item>
-        <el-form-item
-          label="新密码"
-          v-else
-        >
-          <el-input
-            type="password"
-            v-model="tempUser.password"
-            placeholder="不填则表示不修改"
-          >
-          </el-input>
-        </el-form-item>
 
-        <el-form-item
-          label="昵称"
-          required
-        >
-          <el-input
-            type="text"
-            v-model="tempUser.nickname"
-          >
-          </el-input>
-        </el-form-item>
-        <el-form-item
-          label="手机号码"
-          required
-        >
-          <el-input
-            type="text"
-            v-model="tempUser.phonenumber"
-          >
-          </el-input>
-        </el-form-item>
-        <el-form-item
-          label="班级"
-          required
-        >
-          <el-input
-            type="text"
-            v-model="tempUser.stuclass"
-          >
-          </el-input>
-        </el-form-item>
-        <el-form-item
-          label="职位"
-          required
-        >
-          <el-input
-            type="text"
-            v-model="tempUser.position"
-          >
-          </el-input>
-        </el-form-item>
-      </el-form>
-      <div
-        slot="footer"
-        class="dialog-footer"
-      >
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button
-          v-if="dialogStatus==='create'"
-          type="success"
-          @click="createUser"
-        >创 建</el-button>
-        <el-button
-          type="primary"
-          v-else
-          @click="updateUser"
-        >修 改</el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
@@ -210,22 +92,7 @@ export default {
       paginationData: [],
       pagesize: 5,
       keyword: '',
-      stashList: [],
-      dialogFormVisible: false,
-      dialogStatus: 'create',
-      textMap: {
-        create: '新增用户',
-        update: '编辑用户'
-      },
-      tempUser: {
-        id: '',
-        username: '',
-        password: '',
-        nickname: '',
-        phonenumber: '',
-        stuclass: '',
-        position: ''
-      }
+      stashList: []
     }
   },
   created () {
@@ -278,93 +145,6 @@ export default {
         }
       }
       this.total = this.tableData.length
-    },
-    showCreate () {
-      this.dialogStatus = 'create'
-      this.dialogFormVisible = true
-    },
-    showUpdate ($index) {
-      let user = this.list[$index]
-      this.tempUser.id = user.id
-      this.tempUser.username = user.username
-      this.tempUser.password = user.password
-      this.tempUser.nickname = user.nickname
-      this.tempUser.phonenumber = user.phonenumber
-      this.tempUser.stuclass = user.stuclass
-      this.tempUser.position = user.position
-      this.dialogStatus = 'update'
-      this.dialogFormVisible = true
-    },
-    validate (isCreate) {
-      let u = this.tempUser
-      if (isCreate && u.username.trim().length === 0) {
-        this.$message.warning('请填写用户名')
-        return false
-      }
-      if (isCreate && u.password.length === 0) {
-        this.$message.warning('请填写密码')
-        return false
-      }
-      if (u.nickname.trim().length === 0) {
-        this.$message.warning('请填写昵称')
-        return false
-      }
-      if (u.roleIds.length === 0) {
-        this.$message.warning('请选择角色')
-        return false
-      }
-      return true
-    },
-    createUser () {
-      if (!this.validate(true)) return
-      // 添加新用户
-      this.api({
-        url: '/user/addUser',
-        method: 'post',
-        data: this.tempUser
-      }).then(() => {
-        this.$message.success('添加成功')
-        this.getList()
-        this.dialogFormVisible = false
-      })
-    },
-    updateUser () {
-      if (!this.validate(false)) return
-      // 修改用户信息
-      let _vue = this
-      this.api({
-        url: '/user/updateUser',
-        method: 'post',
-        data: this.tempUser
-      }).then(() => {
-        this.$message.success('修改成功')
-        this.dialogFormVisible = false
-        _vue.getList()
-      })
-    },
-    removeUser ($index) {
-      let _vue = this
-      this.$confirm('确定删除此用户?', '提示', {
-        confirmButtonText: '确定',
-        showCancelButton: false,
-        type: 'warning'
-      }).then(() => {
-        let user = _vue.list[$index]
-        user.deleteStatus = '2'
-        _vue
-          .api({
-            url: '/user/updateUser',
-            method: 'post',
-            data: user
-          })
-          .then(() => {
-            this.$message.success('删除成功')
-            _vue.getList()
-          })
-          .catch(() => {
-            _vue.$message.error('删除失败')
-          })
-      })
     }
   }
 }
