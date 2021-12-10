63e7687791706b43c51440d5a90dbcc0a1c4ff78
吴嘉顺 <870435170@qq.com>
2021-12-11 01:28:10 +0800

    Update HomePage.vue

--- test-vue/src/components/home/HomePage.vue
+++ test-vue/src/components/home/HomePage.vue
@@ -2,6 +2,52 @@
   <div>
     登陆成功
     <div>
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <div class="grid-content bg-purple card-panel">
+            <div class="card-panel-icon-wrapper">
+              <div class="card-panel-icon"><i class="el-icon-user"></i></div>
+            </div>
+            <div class="card-panel-description">
+              <div class="card-panel-text">用户数</div>
+              <div class="card-panel-num">{{summary[0].user}}</div>
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple card-panel">
+            <div class="card-panel-icon-wrapper">
+              <div class="card-panel-icon"><i class="el-icon-user"></i></div>
+            </div>
+            <div class="card-panel-description">
+              <div class="card-panel-text">图书库存</div>
+              <div class="card-panel-num">{{summary[0]. book}}</div>
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple card-panel">
+            <div class="card-panel-icon-wrapper">
+              <div class="card-panel-icon"><i class="el-icon-user"></i></div>
+            </div>
+            <div class="card-panel-description">
+              <div class="card-panel-text">未完成订单数</div>
+              <div class="card-panel-num">{{summary[0].order}}</div>
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple card-panel">
+            <div class="card-panel-icon-wrapper">
+              <div class="card-panel-icon"><i class="el-icon-user"></i></div>
+            </div>
+            <div class="card-panel-description">
+              <div class="card-panel-text">总订书量</div>
+              <div class="card-panel-num">{{summary[0].booktotal}}</div>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
@@ -10,7 +56,9 @@
 export default {
   name: 'HomePage',
   data () {
-    return {}
+    return {
+      summary: [{ user: '111', book: '222', order: '333', booktotal: '444' }]
+    }
   },
   created () {},
   methods: {}
@@ -18,4 +66,17 @@ export default {
 </script>
 
 <style scoped>
+.bg-purple {
+  background: #d3dce6;
+}
+.card-panel-icon-wrapper {
+  float: left;
+}
+.card-panel-icon {
+  float: left;
+  font-size: 48px;
+}
+.card-panel-description {
+  float: left;
+}
 </style>
